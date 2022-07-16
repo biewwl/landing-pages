ce80dfc326a62efaf15cc35d302307a4f3057149
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-07-16 13:20:01 -0300

    * Style from homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import page1 from '../../images/page1/page1.png';
+import './styles/Home.css';
 
 function Home() {
   return (
-    <Link to="/page-1">
-      Page 1
-    </Link>
+    <main className="home">
+      <Link to="/page-1" target="_blank">
+        <img src={page1} alt="" />
+      </Link>
+    </main>
   );
 }
 
